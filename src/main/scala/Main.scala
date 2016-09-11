f5cb19dac534414dd2db7e03de5601b5485f83ed
eric <ealang@gmail.com>
2016-09-11 12:04:00 -0500

    Update bot name

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,7 +3,7 @@ import com.jhood.battlebot._
 import scala.io.StdIn.readLine
 
 object Main extends App {
-  val wrappedStrategy = new StdMsgStrategyWrapper(new CalculatedStrategy("random", RandomCalculator))
+  val wrappedStrategy = new StdMsgStrategyWrapper(new CalculatedStrategy("RandoBot", RandomCalculator))
 
   while(true) {
     val response = wrappedStrategy.update(readLine())
